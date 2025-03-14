b8b0664aa1dde54407dce31732c5c0b6173f0766
cats1040 <vshlvani1234@gmail.com>
2025-03-14 17:13:21 +0530

    Login and register user

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,24 @@ require('dotenv').config(); //to access the .env file
 
 const express = require('express');
 const app=express();
+const mongoose = require("mongoose");
+const userRoutes = require("./routes/auth.js")
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+mongoose
+  .connect("mongodb://127.0.0.1:27017/myDatabase")
+  .then(() => {
+    console.log("Mongo connection open");
+  })
+  .catch((err) => {
+    console.log("Mongo connection error! " + err);
+  });
+
+
+app.use("/users", userRoutes);
+
 
 const PORT = process.env.PORT || 5000;
 
